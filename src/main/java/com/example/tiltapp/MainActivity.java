fe5cfe94e352dd18ab8e5cb5bbfa935f4afd2663
William Plummer <kwplummer@gmail.com>
2013-12-11 21:16:00 -0800

    Added different positions to the phone.
    Changed UI to represent these states.
    The app vibrates when a new position is entered.
    We now unregister the sensor when our app hides or exits.

--- src/main/java/com/example/tiltapp/MainActivity.java
+++ src/main/java/com/example/tiltapp/MainActivity.java
@@ -1,42 +1,89 @@
 package com.example.tiltapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
-
+  private String lastSound;
   private String storedValues;
+  SensorEventListener listener;
+
+  private String getStringFromOrientation(double zAzi, double xPitch, double yRoll)
+  {
+    if((xPitch > -25 && xPitch < 25))
+    {
+      if(yRoll > -45 && yRoll < 45)
+      {
+        return "Up";
+      }
+      if(yRoll > 45)
+      {
+        return "Left";
+      }
+      if(yRoll < -45)
+      {
+        return "Right";
+      }
+    }
+    else if((xPitch < -25 && xPitch > -100) && (yRoll > -45 && yRoll < 45))
+    {
+      return "Forward";
+    }
+    else if((xPitch > 25 && xPitch < 130) && (yRoll > -45 && yRoll < 45))
+    {
+      return "Backwards";
+    }
+    else if((xPitch < -100 && xPitch > -160) || (xPitch > 130 && xPitch < 170))
+    {
+      return "Down";
+    }
+    return "None";
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    if(lastSound == null)
+    {
+      lastSound = "";
+    }
     storedValues = "";
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     setContentView(R.layout.activity_main);
     SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    SensorEventListener listener = new SensorEventListener()
+    listener = new SensorEventListener()
     {
       @Override
       public void onSensorChanged(final SensorEvent sensorEvent)
       {
+        String position = getStringFromOrientation(sensorEvent.values[0],
+                sensorEvent.values[1], sensorEvent.values[2]);
+        if(!position.equals("None") && !position.equals(lastSound))
+        {
+          lastSound = position;
+          Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+          v.vibrate(50);
+        }
         runOnUiThread(new Runnable()
         {
           @Override
           public void run()
           {
             TextView text = (TextView) findViewById(R.id.editText);
-            text.setText(String.format("%s, %s, %s\n%s", Float.toString(sensorEvent.values[0]),
-                    Float.toString(sensorEvent.values[1]), Float.toString(sensorEvent.values[2]),
+            text.setText(String.format("%s\n%.2f,\t%.2f,\t%.2f\n%s", lastSound,
+                    sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2],
                     storedValues));
           }
         });
@@ -58,7 +105,28 @@ public class MainActivity extends Activity
         storedValues = text.getText().toString();
       }
     });
-    sensorManager.registerListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-            sensorManager.SENSOR_DELAY_NORMAL);
+  }
+
+  @Override
+  protected void onPause()
+  {
+    super.onPause();
+    if(listener != null)
+    {
+      SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+      sensorManager.unregisterListener(listener);
+    }
+  }
+
+  @Override
+  protected void onResume()
+  {
+    super.onResume();
+    if(listener != null)
+    {
+      SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+      sensorManager.registerListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+              sensorManager.SENSOR_DELAY_FASTEST);
+    }
   }
 }
